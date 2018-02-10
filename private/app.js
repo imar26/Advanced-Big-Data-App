1a6a002af39f4902ab01def9037e3674ae314f07
imar26 <aadeshranderia26@gmail.com>
2018-02-10 15:16:43 -0500

    Revert "store object as hashes and add to sets"
    This reverts commit 4b07914f8acdf598b31e86cb60e48a9aa227f644.

--- private/app.js
+++ private/app.js
@@ -102,7 +102,7 @@ module.exports = function (app, client) {
                 "type": "string"
             }
         },
-        "additionalProperties": false
+        "additionalProperties": false        
     };
 
     // Test data used initially for testing
@@ -159,11 +159,11 @@ module.exports = function (app, client) {
     // };
 
     // Rest API's
-    app.get('/', function (req, res, next) {
+    app.get('/', function(req, res, next) {
         res.send('Hello from nodejs');
     });
 
-    app.post('/addPlan', function (req, res, next) {
+    app.post('/addPlan', function(req, res, next) {
         const uuidv1 = require('uuid/v1');
         let _id = uuidv1();
 
@@ -171,104 +171,17 @@ module.exports = function (app, client) {
         // console.log(addPlan);
 
         let errors = v.validate(addPlan, schema).errors;
-        if (errors.length < 1) {
-            // console.log(addPlan);
-
-            for (let key in addPlan) {
-                // check also if property is not inherited from prototype
-                if (addPlan.hasOwnProperty(key)) {
-                    let value = addPlan[key];
-                    // if(value instanceof Array) {
-                    //     console.log(key);                    
-                    //     console.log(typeof(value));                    
-                    //     console.log('It is an array');                    
-                    // }
-
-                    if(typeof(value) == 'object') {
-
-                        if(value instanceof Array) {
-                            // console.log(value.length);
-                            // console.log(key);
-                            for(let i=0; i<value.length; i++) {
-                                // console.log(value[i]);
-                                let eachValue = value[i];
-                                for (let innerkey in eachValue) {
-                                    if (eachValue.hasOwnProperty(innerkey)) {
-                                        let innerValue = eachValue[innerkey];
-                                        // console.log(innerValue);
-
-                                        if(typeof(innerValue) == 'object') {
-                                            client.hmset("plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i, innerValue, function(err, result) {
-                                                if(err) {
-                                                    console.log(err);
-                                                }
-                                                // console.log(result);                        
-                
-                                                // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                                            });
-
-                                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i);
-                                        } else if(typeof(innerValue) == 'string') {
-                                            client.hset("plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i, innerkey, innerValue, function(err, result) {
-                                                if(err) {
-                                                    console.log(err);
-                                                }
-                                                // console.log(result);                        
-                    
-                                                // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                                            });
-
-                                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i);
-                                        }
-
-                                        // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i);
-                                    }
-                                }
-                            }
-                        } else {
-                            client.hmset("plan-----"+_id+"-----"+key, value, function(err, result) {
-                                if(err) {
-                                    console.log(err);
-                                }
-                                // console.log(result);                        
-
-                                // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                            });
-
-                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                        }
-                    } else if(typeof(value) == 'string') {
-                        client.hset("plan-----"+_id+"-----"+key, key, value, function(err, result) {
-                            if(err) {
-                                console.log(err);
-                            }
-                            // console.log(result);                        
-
-                            // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                        });
-
-                        client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                    }
-
-                    // client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
-                    
-                    // res.send("The key of the newly created plan is: " + "plan-----"+_id+"-----"+key);
+        if(errors.length < 1) {
+            client.set("plan-"+_id, JSON.stringify(addPlan), function(err, result) {
+                if(err) {
+                    console.log(err);
                 }
-            }
-
-            res.sendStatus(201);
-
-
-            // client.set("plan-"+_id, JSON.stringify(addPlan), function(err, result) {
-            //     if(err) {
-            //         console.log(err);
-            //     }
-            //     // console.log(result);
-            //     res.send("The key of the newly created plan is: " + "plan-"+_id);
-            // });
+                // console.log(result);
+                res.send("The key of the newly created plan is: " + "plan-"+_id);
+            });
         } else {
             var errorArray = [];
-            for (let i = 0; i < errors.length; i++) {
+            for(let i=0; i<errors.length; i++) {
                 // console.log(errors[i].stack);
                 errorArray.push(errors[i].stack);
             }
@@ -276,15 +189,11 @@ module.exports = function (app, client) {
         }
     });
 
-    app.get('/getAllPlans', function (req, res, next) {
-        res.send('Displays all the plans.');
-    });
-
-    app.get('/getPlan/:planId', function (req, res, next) {
+    app.get('/getPlan/:planId', function(req, res, next) {
         let planId = req.params.planId;
 
-        client.get(planId, function (err, result) {
-            if (err) {
+        client.get(planId, function(err, result) {
+            if(err) {
                 console.log(err);
             }
             // console.log(JSON.parse(result));
@@ -292,7 +201,7 @@ module.exports = function (app, client) {
         });
     });
 
-    app.delete('/deletePlan/:planId', function (req, res, next) {
+    app.delete('/deletePlan/:planId', function(req, res, next) {
         let planId = req.params.planId;
         client.del(planId);
         res.sendStatus(200);
