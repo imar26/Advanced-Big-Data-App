e7c1dc552c40b54d49d6ceebddd68efee66fff15
imar26 <aadeshranderia26@gmail.com>
2018-03-09 22:32:25 -0500

    decomposing json document into hashmap

--- private/app.js
+++ private/app.js
@@ -1,9 +1,11 @@
 module.exports = function (app, client) {
 
+    // Strong Etag
+    app.set('etag', 'strong');
+
     // Data Validation
     var Validator = require('jsonschema').Validator;
     var v = new Validator();
-
 
     // JSON Schema
     var schema = {
@@ -163,7 +165,7 @@ module.exports = function (app, client) {
         res.send('Hello from nodejs');
     });
 
-    app.post('/addPlan', function(req, res, next) {
+    app.post('/plan', function(req, res, next) {
         const uuidv1 = require('uuid/v1');
         let _id = uuidv1();
 
@@ -180,47 +182,49 @@ module.exports = function (app, client) {
                         if(value instanceof Array) {
                             for(let i=0; i<value.length; i++) {
                                 let eachValue = value[i];
+                                // console.log(eachValue);
                                 for (let innerkey in eachValue) {
                                     if (eachValue.hasOwnProperty(innerkey)) {
                                         let innerValue = eachValue[innerkey];
                                         if(typeof(innerValue) == 'object') {
-                                            client.hmset("plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i, innerValue, function(err, result) {
+                                            client.hmset(_id + "-" + eachValue["objectType"]+"-"+eachValue["objectId"]+"-"+innerkey, innerValue, function(err, result) {
                                                 if(err) {
                                                     console.log(err);
                                                 }
                                             });
-                                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i);
-                                        } else if(typeof(innerValue) == 'string') {
-                                            client.hset("plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i, innerkey, innerValue, function(err, result) {
+                                            // client.sadd(_id + "-" + eachValue["objectType"]+"-"+eachValue["objectId"]+"-"+innerkey, _id + "-" + innerValue["objectType"]+"-"+innerValue["objectId"]);                                            
+                                        } else {
+                                            client.hset(_id + "-" + eachValue["objectType"]+"-"+eachValue["objectId"], innerkey, innerValue, function(err, result) {
                                                 if(err) {
                                                     console.log(err);
                                                 }
                                             });
-                                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key+'------'+innerkey+'-----'+i);
+                                            client.sadd(_id + "-" + addPlan["objectType"]+"-"+addPlan["objectId"]+"-"+key, _id + "-" + eachValue["objectType"]+"-"+eachValue["objectId"]);                                            
                                         }
                                     }
                                 }
                             }
                         } else {
-                            client.hmset("plan-----"+_id+"-----"+key, value, function(err, result) {
-                                if(err) {
-                                    console.log(err);
-                                }
-                            });
-
-                            client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
+                            for (let innerkey in value) {
+                                let innerValue = value[innerkey];
+                                client.hmset(_id + "-" + value["objectType"]+"-"+value["objectId"], value, function(err, result) {
+                                    if(err) {
+                                        console.log(err);
+                                    }
+                                });
+                                // client.sadd(_id + "-" + addPlan["objectType"]+"-"+addPlan["objectId"]+"-"+key, _id + "-" + value["objectType"]+"-"+value["objectId"]);
+                            }                            
                         }
-                    } else if(typeof(value) == 'string') {
-                        client.hset("plan-----"+_id+"-----"+key, key, value, function(err, result) {
+                    } else {
+                        client.hset(_id + "-" + addPlan["objectType"]+"-"+addPlan["objectId"], key, value, function(err, result) {
                             if(err) {
                                 console.log(err);
                             }
                         });
-                        client.sadd("set-----"+_id, "plan-----"+_id+"-----"+key);
                     }
                 }
             }
-            res.status(201).send("The key of the newly created plan is: " + "plan-"+_id);
+            res.status(201).send("The key of the newly created plan is: " + _id);
         } else {
             var errorArray = [];
             for (let i = 0; i < errors.length; i++) {
@@ -234,19 +238,79 @@ module.exports = function (app, client) {
         res.send('Displays all the plans.');
     });
 
-    app.get('/getPlan/:planId', function(req, res, next) {
+    app.get('/plan/:planId', function(req, res, next) {
         let planId = req.params.planId;
+        // console.log(planId);
 
-        client.get(planId, function(err, result) {
+        client.keys(planId+"*", function(err, result) {
             if(err) {
                 console.log(err);
             }
-            if(result) {
-                res.json(JSON.parse(result));
-            } else {
-                res.sendStatus(404);
-            }
+            var finalResult = [];
+
+            client.smembers("8a13fe60-23e1-11e8-b9c1-090d7ee5efa7-plan-12xvxc345ssdsds-planCostShares", function(error, output) {
+                if(error) {
+                    // console.log(error);
+                }
+                // console.log("Before Output");
+                // console.log(output);
+                console.log(output);
+                for(let j=0; j<output.length; j++) {
+                    console.log();
+                }
+                // if(output) {
+                //     finalResult.push(output);
+                // }
+                // res.json(finalResult);
+                // console.log(finalResult);
+
+                // console.log("**********************************");
+                // console.log("After Output");
+            });
+
+            // for(let i=0; i<result.length; i++) {
+                // client.hgetall("8a13fe60-23e1-11e8-b9c1-090d7ee5efa7-plan-12xvxc345ssdsds", function(error, output) {
+                //     if(error) {
+                //         // console.log(error);
+                //     }
+                //     // console.log("Before Output");
+                //     // console.log(output);
+                //     // console.log(output);
+                //     if(output) {
+                //         finalResult.push(output);
+                //     }
+                //     res.json(finalResult[0]);
+                //     // console.log(finalResult);
+
+                //     // console.log("**********************************");
+                //     // console.log("After Output");
+                // });
+            //     // console.log(finalResult);
+            // }
+            // console.log(finalResult);
+            // res.json(finalResult);
         });
+        // client.get(planId, function(err, result) {
+        //     if(err) {
+        //         console.log(err);
+        //     }
+        //     if(result) {
+        //         res.json(JSON.parse(result));
+        //     } else {
+        //         res.sendStatus(404);
+        //     }
+        // });
+
+        // client.hgetall(planId, function(err, result) {
+        //     if(err) {
+        //         console.log(err);
+        //     }
+        //     if(result) {
+        //         res.json(result);
+        //     } else {
+        //         res.sendStatus(404);
+        //     }
+        // });
     });
 
     app.delete('/deletePlan/:planId', function(req, res, next) {
